33160cade9012c0d08a7c45aab24b6d5e05ef32e
Gregor Zeitlinger <gregor.zeitlinger@txtr.com>
2013-06-18 16:10:46 +0200

    release script

--- liquibase-core/release.sh
+++ liquibase-core/release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -B -DaltDeploymentRepository='internal.repo::default::file://${project.build.directory}/mvn-repo' -DskipTests release:prepare release:perform -DtagNameFormat="liquibase-@{project.version}" -DautoVersionSubmodules=true -DpreparationGoals="clean install" -Darguments="-DskipTests" -DuseReleaseProfile=false -Dgoals="clean deploy"
+mvn -B -DskipTests release:prepare release:perform -DtagNameFormat="liquibase-@{project.version}" -DautoVersionSubmodules=true -DpreparationGoals="clean install" -Darguments="-DskipTests" -DuseReleaseProfile=false -Dgoals="clean deploy"
